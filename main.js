989fc66f2afe0adc985a95d502087c26ead26742
Ana Elisa <elisaibz92@gmail.com>
2024-12-12 14:07:26 +0100

    2-4-tipos-de-datos

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 'use strict';
-
+//este ejercicio es para 
 const jsList = document.querySelector('.js-list');
 
 const btn = document.querySelector('.js-btn-add');
 
 const menu = document.querySelector('.js-form');
+//Constantes ejercico 1
+
+const kittenImage1 = "https://dev.adalab.es/gato-siames.webp" ;
+const kittenName1 = "Anastacio" ;
+const kittenRace1 = "Siamés" ;
+const kittenDesc1 = "Porte elegante, su patrón de colorcaracterístico y sus ojos de un azul intenso, pero su historiremonta a Asía al menos hace 500 años, donde tuvo su origenposiblemente.";
+
+
+const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp" ;
+const kittenName2 = "Fiona" ;
+const kittenRace2 = "Sphynx";
+const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+
+const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
+const kittenName3 = "Cielo" ;
+const kittenRace3 = "Maine Coon" ;
+const kittenDesc3 =  "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+
+
+
+
 
 btn.addEventListener('click', ()=>{
     menu.classList.toggle("collapsed");
 });
 const kittenOne = `<li class="card">
 <article>
-      <img class="card_img" src="https://dev.adalab.es/gato-siames.webp" alt="siames-cat" />
-      <h3 class="card_title">Anastacio</h3>
-     <h4 class="card_race">Siamés</h4>
+      <img class="card_img" src="${kittenImage1}" alt="siames-cat" />
+      <h3 class="card_title">${kittenName1}</h3>
+     <h4 class="card_race">${kittenRace1}</h4>
      <p class="card_description">
-         Porte elegante, su patrón de colorcaracterístico y sus ojos
-        de un azul intenso, pero su historiremonta a Asía al menos
-        hace 500 años, donde tuvo su origenposiblemente.
+         ${kittenDesc1}
      </p>
 </article>
 </li>`; 
@@ -75,11 +94,7 @@ console.log(searchDesc)
 
 
 
-const kittenDesc1 = "Porte elegante, su patrón de colorcaracterístico y sus ojos de un azul intenso, pero su historiremonta a Asía al menos hace 500 años, donde tuvo su origenposiblemente."
 
-const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo."
-
-const kittenDesc3 =  "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta."
 
 btnSearch.addEventListener('click', (ev) => {
     ev.preventDefault();
@@ -99,4 +114,9 @@ btnSearch.addEventListener('click', (ev) => {
     }
 
 });
+
+
+
+
+
 
