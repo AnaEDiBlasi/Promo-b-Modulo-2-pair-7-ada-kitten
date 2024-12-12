9c9bf894da226833f46663c5ae14b0eb7ec73516
María del Carmen <jacqueelopez@gmail.com>
2024-12-12 21:57:25 +0100

    agregando comentarios en el codigo

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 'use strict';
-//este ejercicio es para 
+
+//Estamos llamando a las clases de html del boton(+) y a la seccion formulario.//
+const btn = document.querySelector('.js-btn-add');
+const menu = document.querySelector('.js-form');
+
+
+//Estamos llamando a la clase de html de la seccion datos de gatitos// 
 const jsList = document.querySelector('.js-list');
 
-const btn = document.querySelector('.js-btn-add');
 
-const menu = document.querySelector('.js-form');
-//Constantes ejercico 1
-
+//Constantes de cada uno de los gatitos //
+//Constantes de gato 1//
 const kittenImage1 = "https://dev.adalab.es/gato-siames.webp" ;
 const kittenName1 = "Anastacio" ;
 const kittenRace1 = "Siamés" ;
 const kittenDesc1 = "Porte elegante, su patrón de colorcaracterístico y sus ojos de un azul intenso, pero su historiremonta a Asía al menos hace 500 años, donde tuvo su origenposiblemente.";
 
-
+//Constantes de gato 2 //
 const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp" ;
 const kittenName2 = "Fiona" ;
 const kittenRace2 = "Sphynx";
 const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
 
+//Constantes de gato 3
 const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
 const kittenName3 = "Cielo" ;
 const kittenRace3 = "Maine Coon" ;
@@ -25,11 +30,14 @@ const kittenDesc3 =  "Tienen la cabeza cuadrada y los ojos simétricos, por lo q
 
 
 
-
+//Estamos escuchando el evento click, sobre el boton (+) del header. Si el menu esta desplegado se pliega y si esta cerrado se despliega//
 
 btn.addEventListener('click', ()=>{
     menu.classList.toggle("collapsed");
 });
+
+
+//Estamos sustituyendo las variables que declaramos arriba de cada uno de los gatitos//
 const kittenOne = `<li class="card">
 <article>
       <img class="card_img" src="${kittenImage1}" alt="siames-cat" />
@@ -41,7 +49,7 @@ const kittenOne = `<li class="card">
 </article>
 </li>`; 
 
-
+//PENDIENTE DE REEMPLAZO//
 const kittenTwo = `<li class="card">
     <img class="card_img" src="https://dev.adalab.es/sphynx-gato.webp" alt="sphynx-cat" />
      <h3 class="card_title">Fiona</h3>
@@ -53,8 +61,7 @@ const kittenTwo = `<li class="card">
          </p>                   
 </li>`;
 
-
-
+//PENDIENTE DE REEMPLAZO//
 const kittenThree = `<li class= "card">
     <img class="card_img" src="https://dev.adalab.es/maine-coon-cat.webp" alt="maine-coon-cat" />
     <h3 class="card_title">Cielo</h3>
@@ -66,11 +73,14 @@ const kittenThree = `<li class= "card">
     </p>
 </li>`;
 
+
+//Estamos reemplazando el contenido que teniamos en HTML en la seccion datos de gatitos , con las variables que declaramos mas arriba para cada uno//
 jsList.innerHTML = kittenOne;
 jsList.innerHTML += kittenTwo;
 jsList.innerHTML += kittenThree;
 
 
+//Estamos llamando al boton Cancelar y a todos los inputs del formulario "¿agregarmos un nuevo gatito?"//
 const btnCancel = document.querySelector ('.js-btn-cancel');
 const form = document.querySelector('.js-form');
 const url = document.querySelector ('.js-url');
@@ -78,7 +88,7 @@ const name = document.querySelector ('.js-name');
 const raza = document.querySelector ('.js-raza');
 const desc = document.querySelector ('.js-desc');
 
-
+//Estamos escuchando el evento click sobre el boton Cancelar.Añadimos prevent para que al hacer click en enviar no se envie el formulario vacio.Al hacer click en cancelar el formulario se pliega//
 btnCancel.addEventListener('click', (event)=>{
     event.preventDefault();
     form.classList.toggle('collapsed');
@@ -88,13 +98,18 @@ btnCancel.addEventListener('click', (event)=>{
     desc.value = '';
 
 })
+
+//Estamos llamando al boton buscar y al input de la casilla descripcion, en  la seccion filtrado y busqueda//
 const btnSearch = document.querySelector ('.js-button-search');
 const searchDesc = document.querySelector ('.js-in-search-desc');
 console.log(searchDesc)
 
 
+/*Estamos escuchando el evento click sobre el boton BUSCAR.
 
+Agregamos un prevent pero por ahora no funciona.
 
+Estamos haciendo un condicional para que se vean resultados que coicidan con alguna palabra de la descripcion.Si es asi se guarda en la constante valDesc que es igual al VALOR del imput searchDesc*/
 
 btnSearch.addEventListener('click', (ev) => {
     ev.preventDefault();
