65ec1350125cf9678490b46ec9791e1f7fdb56e4
Ana Elisa <elisaibz92@gmail.com>
2024-12-17 10:42:02 +0100

    update functions

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const kittenDesc3 =  "Tienen la cabeza cuadrada y los ojos simétricos, por lo q
 
 
 //Estamos sustituyendo las variables que declaramos arriba de cada uno de los gatitos//
-const kittenOne = `<li class="card">
+/* const kittenOne = `<li class="card">
 <article>
       <img class="card_img" src="${kittenImage1}" alt="siames-cat" />
       <h3 class="card_title">${kittenName1}</h3>
@@ -70,7 +70,7 @@ const kittenThree = `<li class= "card">
     </p>
 </li>`;
 
-
+*/
 //Estamos reemplazando el contenido que teniamos en HTML en la seccion datos de gatitos , con las variables que declaramos mas arriba para cada uno//
 /*jsList.innerHTML = kittenOne;
 jsList.innerHTML += kittenTwo;
@@ -124,3 +124,45 @@ function handleClick() {
 }
 
 btn.addEventListener('click', handleClick);
+
+
+//ejercicio 2 listado//
+function renderKitten(url, name, race, desc){
+    return `<li class= "card">
+    <img class="card_img" src="${url}" />
+    <h3 class="card_title">${name}</h3>
+    <h4 class="card_race">${race}</h4>
+    <p class="card_description">
+             ${desc}
+    </p>
+</li>`;
+    
+  };
+const kittenOne = renderKitten (kittenImage1,kittenName1,kittenRace1, kittenDesc1);
+const kittenTwo = renderKitten  (kittenImage2,kittenName2,kittenRace2, kittenDesc2);
+const kittenThree = renderKitten (kittenImage3,kittenName3,kittenRace3, kittenDesc3);
+jsList.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+
+
+//Filtrar por descripcion//
+/*Estamos escuchando el evento click sobre el boton BUSCAR.
+Agregamos un prevent pero por ahora no funciona.
+Estamos haciendo un condicional para que se vean resultados que coicidan con alguna palabra de la descripcion.Si es asi se guarda en la constante valDesc que es igual al VALOR del imput searchDesc*/
+function filterKitten (ev) {
+    ev.preventDefault();
+    const valDesc = searchDesc.value;
+    
+    if (kittenDesc1.includes(valDesc)) {
+        jsList.innerHTML=kittenOne
+    }
+    
+    if (kittenDesc2.includes(valDesc)) {
+        jsList.innerHTML=kittenTwo
+    }
+    
+    if (kittenDesc3.includes(valDesc)) {
+        jsList.innerHTML=kittenThree
+    }
+}
+btnSearch.addEventListener('click', filterKitten);
